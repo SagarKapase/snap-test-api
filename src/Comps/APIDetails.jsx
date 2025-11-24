17a50d0bab57807ec8e3925d2581c8d65deafcad
SagarKapase <sagarkapase139@gmail.com>
2025-11-24 08:48:50 +0530

    added message

--- src/Comps/APIDetails.jsx
+++ src/Comps/APIDetails.jsx
@@ -1,36 +1,44 @@
-import Card from '@mui/material/Card';
-import Box from '@mui/material/Box';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import GETAPI from './ApiComps/GETAPI';
-import PostAPI from './ApiComps/PostAPI';
-import GetSingleUserAPI from './ApiComps/GetSingleUserAPI';
-import PUTAPI from './ApiComps/PUTAPI';
-import DeleteAPI from './ApiComps/DeleteAPI';
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import GETAPI from "./ApiComps/GETAPI";
+import PostAPI from "./ApiComps/PostAPI";
+import GetSingleUserAPI from "./ApiComps/GetSingleUserAPI";
+import PUTAPI from "./ApiComps/PUTAPI";
+import DeleteAPI from "./ApiComps/DeleteAPI";
 
 export default function APIDetails() {
   return (
-    <Card variant="outlined" sx={{overflow: "auto" }}>
+    <Card variant="outlined" sx={{ overflow: "auto" }}>
       <Box sx={{ p: 2 }}>
         <Stack
           direction="row"
-          sx={{ justifyContent: 'space-between', alignItems: 'center' }}
+          sx={{ justifyContent: "space-between", alignItems: "center" }}
         >
           <Typography gutterBottom variant="h5" component="div">
             Snap-Test.co.in ( V1.0.0 )
           </Typography>
         </Stack>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
           Good for API or automation platforms.
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ color: "text.secondary", mt: 1, fontWeight: "bold" }}
+        >
+          Note : “We’re still building a few features. The site is under
+          development — sorry for the inconvenience and thanks for sticking with
+          us!”
         </Typography>
       </Box>
       <Divider />
       <Box sx={{ p: 2 }}>
         <Stack direction="column" spacing={1}>
           <GETAPI />
-            <Divider sx={{ mt: "40px", mb: "40px" }} />
+          <Divider sx={{ mt: "40px", mb: "40px" }} />
           <PostAPI />
           <Divider sx={{ mt: "40px", mb: "40px" }} />
           <GetSingleUserAPI />
